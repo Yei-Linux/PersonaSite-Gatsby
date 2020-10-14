532c0a2188164b84225f8ee3053bd2e30ebd7525
Jesus <jesusalvan2010@hotmail.com>
2020-10-14 13:01:45 -0500

    Test timeout issue :mag:

--- src/lambda/email.js
+++ src/lambda/email.js
@@ -2,11 +2,10 @@ import notificationAxiosClient from "../config/notificationAxios"
 
 export function handler(event,context, callback){
     let emailRequest = JSON.parse(event.body);
-    console.log(emailRequest,process.env.GATSBY_NOTIFICATION_URL);
     notificationAxiosClient.post("email/send",emailRequest).then((response) => {
         console.log(response);
     })
-    callback(null, {
+    context.done(null, {
         statusCode: 200, 
         body: JSON.stringify({
           msg: "Response sent"+process.env.GATSBY_NOTIFICATION_URL,
