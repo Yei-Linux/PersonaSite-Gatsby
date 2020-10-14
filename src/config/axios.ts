f0bdf37226edb679571f6713dfbd0336a18b4711
Jesus <jesusalvan2010@hotmail.com>
2020-10-14 12:27:24 -0500

    Change env variables :rocket:

--- src/config/axios.ts
+++ src/config/axios.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosClient = axios.create({
-    baseURL: process.env.GATSBY_LAMBDA_URL
+    baseURL: `${process.env.GATSBY_LAMBDA_URL}/.netlify/functions`
 });
 
 export default axiosClient;
