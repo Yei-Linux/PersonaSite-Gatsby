73a550c5b9b860024cd7d785ac6a2fd4ccd4bcf5
Jesus <jesusalvan2010@hotmail.com>
2020-10-14 13:30:59 -0500

    Test event header in lambda function :mag:

--- src/lambda/email.js
+++ src/lambda/email.js
@@ -8,7 +8,7 @@ export function handler(event,context, callback){
     callback(null, {
         statusCode: 200, 
         body: JSON.stringify({
-          msg: context
+          msg: event.headers
         })
     })
 }
