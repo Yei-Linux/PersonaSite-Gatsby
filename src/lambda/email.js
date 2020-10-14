6259bbe9333db116ef710de520b62387358960a0
Jesus <jesusalvan2010@hotmail.com>
2020-10-14 12:45:34 -0500

    Adding axios config in email lambda :rocket:

--- src/lambda/email.js
+++ src/lambda/email.js
@@ -1,15 +1,15 @@
-//import axiosClient from "../config/axios"
+import notificationAxiosClient from "../config/notificationAxios"
 
 export function handler(event,context, callback){
     let emailRequest = JSON.parse(event.body);
     console.log(emailRequest,process.env.GATSBY_NOTIFICATION_URL);
-    /*axiosClient.post("email/send",emailRequest).then((response) => {
+    notificationAxiosClient.post("email/send",emailRequest).then((response) => {
         console.log(response);
-    })*/
+    })
     callback(null, {
         statusCode: 200, 
         body: JSON.stringify({
-          msg: "Response sent",
+          msg: "Response sent"+process.env.GATSBY_NOTIFICATION_URL,
         }),
     })
 }
