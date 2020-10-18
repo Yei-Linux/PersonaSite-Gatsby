e802d2f91ad326be91123c65afc57658116a610c
Jesus <jesusalvan2010@hotmail.com>
2020-10-17 19:20:17 -0500

    Cors lambda changes :rocket:

--- src/lambda/email.js
+++ src/lambda/email.js
@@ -1,14 +1,21 @@
 import notificationAxiosClient from "../config/notificationAxios"
 
 export function handler(event,context, callback){
+    const headers = {
+        'Access-Control-Allow-Origin': '*',
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
+      };
+
     let emailRequest = JSON.parse(event.body);
     notificationAxiosClient.post("email/send",emailRequest).then((response) => {
         console.log(response);
     })
     callback(null, {
         statusCode: 200, 
+        headers,
         body: JSON.stringify({
-          msg: "Message sent successful"
+          msg: event.headers
         })
     })
 }
