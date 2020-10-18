155e5116f2e808a6c97502503c9af3f11d1d03ee
Jesus <jesusalvan2010@hotmail.com>
2020-10-17 20:44:51 -0500

    Testing restricting lambda :rocket:

--- src/lambda/email.js
+++ src/lambda/email.js
@@ -1,21 +1,28 @@
 import notificationAxiosClient from "../config/notificationAxios"
 
-export function handler(event,context, callback){
-    const headers = {
-        'Access-Control-Allow-Origin': '*',
-        'Access-Control-Allow-Headers': 'Content-Type',
-        'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE'
-    };
+export function handler(event, context, callback) {
+  const headers = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Headers": "Content-Type",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE",
+  }
 
-    let emailRequest = JSON.parse(event.body);
-    notificationAxiosClient.post("email/send",emailRequest).then((response) => {
-        console.log(response);
-    })
-    callback(null, {
-        statusCode: 200, 
-        headers,
-        body: JSON.stringify({
-          msg: event.headers
-        })
-    })
+  if (event.httpMethod !== 'POST') {
+    return {
+       statusCode: 500,
+       body: 'only by site'
+    }
+ }
+
+  let emailRequest = JSON.parse(event.body)
+  notificationAxiosClient.post("email/send", emailRequest).then(response => {
+    console.log(response)
+  })
+  callback(null, {
+    statusCode: 200,
+    headers,
+    body: JSON.stringify({
+      msg: event.headers,
+    }),
+  })
 }
